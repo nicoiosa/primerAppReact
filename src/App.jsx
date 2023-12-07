a6a2f59aa2d60fa82e9e9ff38784b1d5189c30d1
nicoIosa <nicolasiosa@hotmail.com>
2023-12-07 11:13:36 -0300

    agregamos informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Information from "./components/Information";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
+  // aqui agregamos la logica del componente
   return (
-    <section>
-      <h1>HOHOLA</h1>
+    // aqui puede ir un poco de logica
+    // luego va el resto del componente
+    <section className="container">
+      <h1 className="display-3 my-5">Primer App React</h1>
+      {/* <Information></Information> */}
       <Information />
     </section>
   );
