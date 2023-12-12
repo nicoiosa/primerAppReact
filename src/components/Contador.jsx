9aa111b59c0a6595807564e02cc4a64da8b642f6
nicoIosa <nicolasiosa@hotmail.com>
2023-12-12 10:35:11 -0300

    agregado boton para decrementar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -7,8 +7,11 @@ const Contador = () => {
     <section className="text-center">
       <h3>Contador</h3>
       <p className="fw-bold">{numero}</p>
-      <button className="btn btn-primary" onClick={() => setNumero(numero + 1)}>
+      <button className="btn btn-primary me-2git" onClick={() => setNumero(numero + 1)}>
         +1
+      </button>
+      <button className="btn btn-primary" onClick={() => setNumero(numero - 1)}>
+        -1
       </button>
     </section>
   );
