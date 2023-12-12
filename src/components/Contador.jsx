147c18e1454cac0d6e12afc24ad50194bcd3bf33
nicoIosa <nicolasiosa@hotmail.com>
2023-12-12 10:33:54 -0300

    agregado state

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,16 +1,13 @@
+import { useState } from "react";
+
 const Contador = () => {
-  let numero = 10;
+  // let numero = 10;
+  const [numero, setNumero] = useState(10);
   return (
     <section className="text-center">
       <h3>Contador</h3>
       <p className="fw-bold">{numero}</p>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          numero++;
-          console.log(numero);
-        }}
-      >
+      <button className="btn btn-primary" onClick={() => setNumero(numero + 1)}>
         +1
       </button>
     </section>
